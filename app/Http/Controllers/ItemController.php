c5cffb6e2608c8e25338d68c7f6f498a7199294b
Nykilor <smigi1997@gmail.com>
2020-09-18 12:38:51 +0200

    Small change to the way ItemController handles user filtration, added filtration by item_category_id

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
-    public function index(Request $request)
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function index()
     {
         $user = Auth::user();
         $user_category_access = $user->getUserCategoryAccess('read');
@@ -21,20 +26,20 @@ class ItemController extends Controller
             'person',
             'localization',
             'subLocalization',
-            'itemCategory'
-        ])->whereHas('itemCategory', function($query) use($user_category_access, $request)
+        ])->whereHas('itemCategory', function($query) use($user_category_access)
         {
-            $query->whereIn('id', $user_category_access);
+            $this->addUserFilteringToDataFetch($query, 'item_category_id', 'whereIn', ['category_id']);
+            $query->whereIn('category_id', $user_category_access);
         });
         //TODO repository pattern
         //Handle all the non-array user filtering queries
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'person_id', ['id', '=']);
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'localization_id', ['id', '=']);
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'sub_localization_id', ['id', '=']);
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'model', ['model', 'LIKE']);
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'serial', ['serial', 'LIKE']);
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'producer', ['producer', 'LIKE']);
-        $this->addWhereToFetchBasedOnUserByIdQuery($items, $request, 'inside_identifier', ['inside_identifier', 'LIKE']);
+        $this->addUserFilteringToDataFetch($items, 'person_id', 'where', ['id', '=']);
+        $this->addUserFilteringToDataFetch($items, 'localization_id', 'where', ['id', '=']);
+        $this->addUserFilteringToDataFetch($items, 'sub_localization_id', 'where', ['id', '=']);
+        $this->addUserFilteringToDataFetch($items, 'model', 'where', ['model', 'LIKE']);
+        $this->addUserFilteringToDataFetch($items, 'serial', 'where', ['serial', 'LIKE']);
+        $this->addUserFilteringToDataFetch($items, 'producer', 'where', ['producer', 'LIKE']);
+        $this->addUserFilteringToDataFetch($items, 'inside_identifier', 'where', ['inside_identifier', 'LIKE']);
 
         return ItemListResource::collection($items->get());
     }
@@ -48,20 +53,36 @@ class ItemController extends Controller
             'person',
             'localization',
             'subLocalization',
-            'itemCategory'
         ])->whereHas('itemCategory', function($query) use($user_category_access) {
             $query->whereIn('id', $user_category_access);
         })->where('id', '=', $id)->firstOrFail();
 
         return ItemShowResurce::make($item);
-
     }
 
-    protected function addWhereToFetchBasedOnUserByIdQuery(Builder &$builder, Request $request, string $user_query, array $where)
+    /**
+     * Calls a given method ($func) on $builder with user data given in url query parameter with the spread of given array of $parameters
+     * @param Builder $builder
+     * @param string $user_query $_GET key
+     * @param string $func f.i where, whereIn etc
+     * @param array $parameters f.i ['id', '='], ['category_id']
+     */
+    protected function addUserFilteringToDataFetch(Builder &$builder, string $user_query, string $func, array $parameters)
     {
-        if($where[] = $request->query($user_query))
+        $request = $this->request;
+
+        if($user_query_value = $request->query($user_query))
         {
-            $builder->where(...$where);
+            if(strpos($user_query_value, '[') === 0 && strpos($user_query_value, ']') > 1)
+            {
+                $parameters[] = array_filter(explode(',', str_replace(['[',']'], "", $user_query_value)));
+            }
+            else
+            {
+                $parameters[] = $user_query_value;
+            }
+
+            $builder->$func(...$parameters);
         }
     }
 }
